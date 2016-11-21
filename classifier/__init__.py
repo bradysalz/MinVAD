52b0b08c3990e810ffa431f210d35065dd32af84
Brady Salz <brady.salz@gmail.com>
2016-11-21 18:53:46 -0500

    config files and another init

---
+++ classifier/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 18:52:03 2016
+
+@author: brady
+"""
---
+++ config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 18:51:11 2016
+
+@author: brady
+"""
+
+ROOT_CLEAN = r'C:\Users\brady\GitHub\MinVAD\data\positive\clean'
+ROOT_KITCHEN = r'C:\Users\brady\GitHub\MinVAD\data\negative\building_106_kitchen\training_segments'
+
