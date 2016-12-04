017bdfd40170050363220707bd7538c42216cc36
Brady Salz <brady.salz@gmail.com>
2016-12-04 14:52:36 -0600

    data clean up, separate into train/test

--- feature_extract/config.py
+++ feature_extract/config.py
@@ -5,12 +5,19 @@ Created on Mon Nov 21 18:51:11 2016
 @author: brady
 """
 
-ROOT_CLEAN = r'C:\Users\brady\GitHub\MinVAD\data\positive\clean'
-ROOT_AN4 = r'C:\Users\brady\GitHub\MinVAD\data\positive\an4_clstk'
-ROOT_CLEAN_AN4 = r'C:\Users\brady\GitHub\MinVAD\data\positive\clean_an4clstk'
+# POS DIRS
+TRAIN_CLEAN = r'C:\Users\brady\GitHub\MinVAD\data\positive\clean'
+TRAIN_0DB = r'C:\Users\brady\GitHub\MinVAD\data\positive\0db'
+TRAIN_5DB = r'C:\Users\brady\GitHub\MinVAD\data\positive\5db'
+TRAIN_10DB = r'C:\Users\brady\GitHub\MinVAD\data\positive\10db'
+TRAIN_15DB = r'C:\Users\brady\GitHub\MinVAD\data\positive\15db'
+TRAIN_AN4 = r'C:\Users\brady\GitHub\MinVAD\data\train\positive\an4_clstk'
+TRAIN_MSAK = r'C:\Users\brady\GitHub\MinVAD\data\train\positive\msak0'
+TRAIN_FSEW = r'C:\Users\brady\GitHub\MinVAD\data\train\positive\fsew0'
 
+# NEG DIRS
 ROOT_KITCHEN = r'C:\Users\brady\GitHub\MinVAD\data\negative\building_106_kitchen\training_segments'
 ROOT_URBAN = r'C:\Users\brady\GitHub\MinVAD\data\negative\UrbanSound\data'
 
-POS_DIRS = [ROOT_CLEAN, ROOT_AN4]
+POS_DIRS = [TRAIN_CLEAN, TRAIN_0DB, TRAIN_10DB, TRAIN_15DB, TRAIN_AN4, TRAIN_MSAK, TRAIN_FSEW]
 NEG_DIRS = [ROOT_KITCHEN, ROOT_URBAN]
